a71cac466bbc2c505d47e7c78183d651a31ac3a4
metacolon <timo@teemze.de>
2021-12-24 02:14:41 +0100

    23 2
    Removed test

--- 23.c
+++ 23.c
@@ -38,8 +38,8 @@ int main()
 {
     FILE *in = fopen("in23", "r");
 
-    //printf("Part1: %d\n", part1(in));
-    //rewind(in);
+    printf("Part1: %d\n", part1(in));
+    rewind(in);
     printf("Part2: %d\n", part2(in));
 
     fclose(in);
@@ -226,14 +226,14 @@ void fillPods2(FILE *in, int *pods)
             roomPods+0, roomPods+1, roomPods+2, roomPods+3,
             roomPods+12, roomPods+13, roomPods+14, roomPods+15) == POP1*TYPES ||
         fprintf(stderr, "Couldn't read pods\n");
-    roomPods[4] = 'A';//'D';
-    roomPods[5] = 'B';//'C';
-    roomPods[6] = 'C';//'B';
-    roomPods[7] = 'D';//'A';
-    roomPods[8] = 'A';//'D';
-    roomPods[9] = 'B';//'B';
-    roomPods[10] = 'C';//'A';
-    roomPods[11] = 'D';//'C';
+    roomPods[4] = /*'A';*/'D';
+    roomPods[5] = /*'B';*/'C';
+    roomPods[6] = /*'C';*/'B';
+    roomPods[7] = /*'D';*/'A';
+    roomPods[8] = /*'A';*/'D';
+    roomPods[9] = /*'B';*/'B';
+    roomPods[10] = /*'C';*/'A';
+    roomPods[11] = /*'D';*/'C';
     // Normalize
     for (int i = 0; i < POP*TYPES; i++)
         roomPods[i] -= 'A';
